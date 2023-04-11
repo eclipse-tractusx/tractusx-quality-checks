ca827b3839280b5d5c95a8d0637940934e663333
Carsten Lenz <carsten.lenz@mercedes-benz.com>
2023-04-11 12:05:24 +0200

    chore(doc): moved doc for methods to interface
    The doc for methods implied by the interface moved to interface documentation.

--- internal/install_exists.go
+++ internal/install_exists.go
@@ -21,43 +21,26 @@ package txqualitychecks
 
 import "os"
 
-// InstallExists defines a struct for
 type InstallExists struct {
 }
 
-// NewInstallExists func return a new check based on QualityGuideline interface.
+// NewInstallExists returns a new check based on QualityGuideline interface.
 func NewInstallExists() QualityGuideline {
 	return &InstallExists{}
 }
 
-// Name implements ExternalDescription from interface
-// QualityGuideline and returns the name of the test.
 func (r *InstallExists) Name() string {
 	return "TRG 1.02 - INSTALL.md"
 }
 
-// Description implements ExternalDescription from interface
-// QualityGuideline and returns a brief description of this test.
 func (r *InstallExists) Description() string {
 	return "File INSTALL.md contains comprehensive instructions for installation."
 }
 
-// ExternalDescription implements ExternalDescription from interface
-// QualityGuideline and returns an external description like a URL with further
-// details for this test.
 func (r *InstallExists) ExternalDescription() string {
 	return "https://eclipse-tractusx.github.io/docs/release/trg-1/trg-1-2"
 }
 
-// IsOptional implements IsOptional from interface QualityGuideline to control if
-// the test is optional.
-func (r *InstallExists) IsOptional() bool {
-	return true
-}
-
-// Test implements the test if optional file INSTALL.md exists. If file is
-// missing an error description is added to QualityResult and Passed set to
-// false, other it returns QualityResult Passed true.
 func (r *InstallExists) Test() *QualityResult {
 	_, err := os.Stat("INSTALL.md")
 
@@ -67,4 +50,8 @@ func (r *InstallExists) Test() *QualityResult {
 		}
 	}
 	return &QualityResult{Passed: true}
+}
+
+func (r *InstallExists) IsOptional() bool {
+	return true
 }
